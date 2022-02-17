2790ce44bb4c50c144ff24cab1eb5ff991ad5a01
Emma Kanewoff <emmakanewoff@gmail.com>
2022-02-17 13:28:18 +0100

    added card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+const Card = () => {
+    return ( 
+        <div style={{marginTop: "195px"}}>
+            {/* <div style={{border: "1px solid black", backgroundColor: "gainsboro", padding: "40px 80px 40px 80px", margin: "15px"}}>
+
+            </div> */}
+            <span style={{border: "1px solid black", backgroundColor: "gainsboro", padding: "40px 80px 40px 80px"}}>Card</span>
+        </div>
+     );
+}
+ 
+export default Card;
