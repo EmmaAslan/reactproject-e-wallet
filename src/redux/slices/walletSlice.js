0ce5be68fb985a3b407cd6b1be33e62334cddaa4
Lina Jepsen <linajepsen@live.se>
2022-02-21 16:48:56 +0100

    Testar två arrayer - ej klar

--- src/redux/slices/walletSlice.js
+++ src/redux/slices/walletSlice.js
@@ -15,15 +15,36 @@ const walletSlice = createSlice({
         ifActive: true,
       },
     ],
+    inActiveCards: [
+      {
+        ifActive: false,
+      },
+    ],
     latestId: 1,
+    //isActive: true,
   },
   reducers: {
     addCard: (state, action) => {
-      state.listOfCards.push(action.payload);
+      state.inActiveCards.push(action.payload);
       state.latestId += 1;
     },
+    /*changeActive: (state, action) => {
+      state.listOfCards = state.listOfCards.filter(
+        (card) => card.ifActive === false
+      );
+    },*/
+    changeActive: (state, action) => {
+      state.listOfCards.push(action.payload);
+    },
+    /*changeActive: (state, action) => {
+      state.listOfCards.sort(action.payload);
+      state.latestId = 1;
+    },*/
+    /*changeActive: (state) => {
+      state.isActive = !state.isActive;
+    },*/
   },
 });
 
-export const { addCard } = walletSlice.actions;
+export const { addCard, changeActive } = walletSlice.actions;
 export default walletSlice.reducer;
