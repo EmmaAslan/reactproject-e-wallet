197a97c547ae615f7c511f37692f194d540fcbc1
Lina Jepsen <linajepsen@live.se>
2022-02-22 12:01:16 +0100

    testing two arrays

--- src/redux/slices/walletSlice.js
+++ src/redux/slices/walletSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const walletSlice = createSlice({
   name: "walletList",
   initialState: {
-    listOfCards: [
+    activeCard: [
       {
         cardNumber: "1231 1231 1231 1231",
         firstName: "React",
@@ -15,25 +15,43 @@ const walletSlice = createSlice({
         ifActive: true,
       },
     ],
-    inActiveCards: [
-      {
-        ifActive: false,
-      },
-    ],
+    notActiveCards: [],
     latestId: 1,
     //isActive: true,
   },
   reducers: {
     addCard: (state, action) => {
-      state.inActiveCards.push(action.payload);
+      state.notActiveCards.push(action.payload);
       state.latestId += 1;
     },
-    /*changeActive: (state, action) => {
-      state.listOfCards = state.listOfCards.filter(
-        (card) => card.ifActive === false
-      );
-    },*/
     changeActive: (state, action) => {
+      const change = state.activeCard.pop(action.payload);
+      state.notActiveCards.push(change);
+      const change2 = state.notActiveCards.shift(
+        (card) => card.id === action.payload
+      );
+      state.activeCard.push(change2);
+    },
+    /*changeActive: (state, action) => {
+      state.notActive = [...state.listOfCards, action.payload];
+      state.listOfCards.pop();
+      state.listOfCards = [...state.notActive, action.payload];
+      state.notActive.push();
+    },*/
+    /*changeActive: (state, action) => {
+      const index = state.finIndex((card) => card.id === action.payload.id);
+      state[index].ifActive = action.payload.ifActive;
+    },*/
+
+    /*changeActive: (state, action) => {
+      const change = state.listOfCards.shift(action.payload);
+      state.notActive.push(change);
+    },*/
+    /* changeActive: (state, payload) => {
+      const changeActive = state.find((card) => card.id === payload.id);
+      if (changeActive) changeActive.ifActive = payload.ifActive;
+    },*/
+    /*changeActive: (state, action) => {
       state.listOfCards.push(action.payload);
     },
     /*changeActive: (state, action) => {
