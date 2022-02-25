fbb607950ed84267c55c00786b32161bda8e2b5b
Lina Jepsen <linajepsen@live.se>
2022-02-25 11:27:31 +0100

    fetch api fixa

--- src/redux/slices/walletSlice.js
+++ src/redux/slices/walletSlice.js
@@ -1,20 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const getRandomUser = createAsyncThunk(
+  "walletList/getRandomUser",
+  async () => {
+    let response = await fetch("https://randomuser.me/api/");
+    let json = await response.json();
+    return json;
+  }
+);
 
 const walletSlice = createSlice({
   name: "walletList",
   initialState: {
+    randomUser: {
+      firstName: "",
+      lastName: "",
+    },
     listOfCards: [
       {
         id: Date.now(),
         cardNumber: "4578 7854 4522 1336",
-        firstName: "React",
-        lastName: "von Projektsson",
+        randomUser: {
+          firstName: "",
+          lastName: "",
+        },
         validMonth: "07",
         validYear: "27",
         cvc: "412",
         vendor: "LE Bank",
       },
     ],
+    status: null,
   },
   reducers: {
     addCard: (state, { payload }) => {
@@ -32,6 +48,14 @@ const walletSlice = createSlice({
         (card) => card.id !== payload
       );
       return { ...state, listOfCards: filteredCards };
+    },
+  },
+  extraReducers: {
+    [getRandomUser.fulfilled]: (state, { payload }) => {
+      state.randomUser.firstName = payload.results[0].name.first.toUpperCase();
+      state.listOfCards[0].randomUser.firstName =
+        payload.results[0].name.first.toUpperCase();
+      state.status = null;
     },
   },
 });
